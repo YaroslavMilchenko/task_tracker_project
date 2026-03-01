f954f19dfaa9bdef246d4c022a7d20cbdc858fb5
Yaroslav Milchenko <zadirakalena@gmail.com>
2026-03-01 17:14:28 +0200

    Add SQLAlchemy models + initial Alembic nigration

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    # Build the database connection string
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    # Load variables from the .env file
+    model_config = SettingsConfigDict(env_file=".env")
+
+# Create a global settings object
+settings = Settings()
